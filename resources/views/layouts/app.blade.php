f20f4684fd79d166ad33504477757bd961cf81e7
Thimon Wentink <t.wentink@programic.com>
2021-09-10 14:14:02 +0200

    Initial header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
         <title>{{ config('app.name') }}</title>
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{ url('css/tailwind.css') }}">
         <style>[x-cloak] { display: none !important; }</style>
         @livewireStyles
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
@@ -19,7 +20,15 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
 
-    <body class="antialiased">
-        {{ $slot }}
-    </body>
+    <header class="relative bg-indigo-800">
+        <div class="absolute inset-0">
+            <img class="w-full h-full object-cover" src="{{ url('img/background.jpg') }}" alt="">
+            <div class="absolute inset-0 bg-indigo-800 mix-blend-multiply" aria-hidden="true"></div>
+        </div>
+
+        <div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+            <h1 class="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">Programic</h1>
+            <p class="mt-6 text-xl text-indigo-100 max-w-3xl">Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque lacus nisi urna, arcu sociis eu. Orci vel lectus nisl eget eget ut consectetur. Sit justo viverra non adipisicing elit distinctio.</p>
+        </div>
+    </header>
 </html>
