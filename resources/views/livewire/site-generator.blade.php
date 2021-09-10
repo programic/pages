32b9355aef598689a542c5c7373e6f92f98e423c
Daan Kloetstra <daankloetstra@hotmail.com>
2021-09-10 15:24:25 +0200

    Reset form

--- resources/views/livewire/site-generator.blade.php
+++ resources/views/livewire/site-generator.blade.php
@@ -1,8 +1,13 @@
 <div class="max-w-7xl mx-auto py-4">
     <form wire:submit.prevent="submit">
         {{ $this->form }}
-        <button type="submit" class="mt-5 py-2 px-4 rounded-md text-white bg-blue-500 hover:bg-blue-700">
-            {{ __('Save site') }}
-        </button>
+        <div class="flex space-x-2">
+            <button type="submit" class="mt-5 py-2 px-4 rounded-md text-white bg-blue-500 hover:bg-blue-700">
+                {{ __('Save site') }}
+            </button>
+            <button type="button" wire:click="resetForm" class="mt-5 py-2 px-4 rounded-md text-white bg-red-500 hover:bg-red-700">
+                {{ __('Reset') }}
+            </button>
+        </div>
     </form>
 </div>
