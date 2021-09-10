6fdd0215718107ea2997fe37e38c4e660ada13e0
Daan Kloetstra <daankloetstra@hotmail.com>
2021-09-10 14:36:06 +0200

    Create site form

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/sites/create', \App\Http\Livewire\SiteGenerator::class);
+
 Route::get('/', function () {
     return view('welcome');
 });
